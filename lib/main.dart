caf7c59e4d734cd92037491cb939b57ce077bc0d
Fajar Tri Haryanto <ftfajar5@gmail.com>
2021-06-12 14:06:41 +0900

    update in UI

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,8 @@ class _MyInputState extends State<MyInput> {
   convertToTelegram(text) {
     text = text.replaceAll('_', '__');
     text = text.replaceAll('*', '**');
+    text = text.replaceAll('~', '~~');
+    text = text.replaceAll('*', '**');
     return text;
   }
 
@@ -64,32 +66,35 @@ class _MyInputState extends State<MyInput> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('WhatsApp Markdown to Telegram Markdown'),
+        title: Center(child: Text('WhatsApp Markdown to Telegram Markdown')),
+        toolbarHeight: 100,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.fromLTRB(300, 50, 300, 0),
         child: Center(
-          child: Column(children: <Widget>[
-            TextField(
-              controller: myController,
-              keyboardType: TextInputType.multiline,
-              maxLines: 7,
-              onChanged: (value) =>
-                  myController2.text = convertToTelegram(myController.text),
-            ),
-            TextField(
-              controller: myController2,
-              keyboardType: TextInputType.multiline,
-              maxLines: 7,
-            ),
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                onPressed: _copyToClipboard,
-                child: Text('Copy'),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: myController,
+                keyboardType: TextInputType.multiline,
+                maxLines: 7,
+                onChanged: (value) =>
+                    myController2.text = convertToTelegram(myController.text),
               ),
-            ),
-          ]),
+              TextField(
+                controller: myController2,
+                keyboardType: TextInputType.multiline,
+                maxLines: 7,
+              ),
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: _copyToClipboard,
+                  child: Text('Copy'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
